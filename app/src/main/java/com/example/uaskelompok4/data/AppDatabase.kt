e3f37735382f33a940189a4d8fd5f0e8e1e63ac5
Graceoliviaa <graceoliviahutagalung@gmail.com>
2026-07-09 02:59:30 +0700

    Integrate document module and fix Room database

--- app/src/main/java/com/example/uaskelompok4/data/AppDatabase.kt
+++ app/src/main/java/com/example/uaskelompok4/data/AppDatabase.kt
@@ -6,16 +6,31 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.uaskelompok4.data.dao.ClientDao
 import com.example.uaskelompok4.data.dao.ProjectDao
+import com.example.uaskelompok4.data.dao.ProjectDocumentDao
 import com.example.uaskelompok4.data.entity.Client
 import com.example.uaskelompok4.data.entity.Project
+import com.example.uaskelompok4.data.entity.ProjectDocument
 
-@Database(entities = [Project::class, Client::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        Project::class,
+        Client::class,
+        ProjectDocument::class
+    ],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun projectDao(): ProjectDao
+
     abstract fun clientDao(): ClientDao
 
+    abstract fun projectDocumentDao(): ProjectDocumentDao
+
     companion object {
-        @Volatile private var INSTANCE: AppDatabase? = null
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
